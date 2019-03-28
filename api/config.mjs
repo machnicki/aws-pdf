98f7cbe2f807d462f2c32e5926886b786abb7c0b
Miron Machnicki <miron@silesis.io>
2019-03-28 16:03:21 +0100

    Don't use keys in lambda config

--- api/config.mjs
+++ api/config.mjs
@@ -1,25 +1,32 @@
 const useLocal = process.env.LOCALSTACK_HOSTNAME || (process.env.NODE_ENV !== 'production')
+const isLambda = !!process.env.LAMBDA
 
 const DynamoDB = {
   region: 'eu-west-2',
   endpoint: useLocal ? `http://${process.env.LOCALSTACK_HOSTNAME || 'localhost'}:4569` : undefined,
-  accessKeyId: useLocal ? 'access-key' : process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: useLocal ? 'secret-key' : process.env.AWS_SECRET_ACCESS_KEY,
+  ...isLambda ? {} : {
+    accessKeyId: useLocal ? 'access-key' : process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: useLocal ? 'secret-key' : process.env.AWS_SECRET_ACCESS_KEY,
+  },
 }
 
 const S3 = {
   region: 'eu-west-2',
-  accessKeyId: useLocal ? 'access-key' : process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: useLocal ? 'secret-key' : process.env.AWS_SECRET_ACCESS_KEY,
   endpoint: useLocal ? `http://${process.env.LOCALSTACK_HOSTNAME || 'localhost'}:4572` : undefined,
   s3ForcePathStyle: true,
+  ...isLambda ? {} : {
+    accessKeyId: useLocal ? 'access-key' : process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: useLocal ? 'secret-key' : process.env.AWS_SECRET_ACCESS_KEY,
+  },
 }
 
 const Lambda = {
   endpoint: useLocal ? `http://${process.env.LOCALSTACK_HOSTNAME || 'localhost'}:4574` : undefined,
   region: 'eu-west-2',
-  accessKeyId: useLocal ? 'access-key' : process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: useLocal ? 'secret-key' : process.env.AWS_SECRET_ACCESS_KEY,
+  ...isLambda ? {} : {
+    accessKeyId: useLocal ? 'access-key' : process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: useLocal ? 'secret-key' : process.env.AWS_SECRET_ACCESS_KEY,
+  },
 }
 
 const config = {
